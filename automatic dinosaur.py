db6e61ad605a487aa008fedf9455bb8e814a7221
Pawan Kumar <41236943+0701pawan@users.noreply.github.com>
2020-05-12 15:20:08 +0530

    Update automatic dinosaur.py

--- automatic dinosaur.py
+++ automatic dinosaur.py
@@ -2,14 +2,16 @@ import pyautogui
 from PIL import Image, ImageGrab
 import time
 
+### The below function automatically presses the key which is given to it as an argument #### 
 def hit(key):
     pyautogui.keyDown(key)
 
-
+### The below function takes the screenshot of the desktop ##
 def takescreenshot():
     image = ImageGrab.grab()
     return image
 
+#### This function checks if data in the frame is black or not if it is black it automatically jumps ####
 def isCollid(data):
     for i in range(760,780):
         for j in range(220,260):
@@ -21,22 +23,10 @@ if __name__=="__main__":
     time.sleep(2)
     hit("up")
 
-
+### this is a continues loop ####
     while True:
         image = ImageGrab.grab().convert('L')
         data=image.load()
         if isCollid(data):
             hit("up")
-        # for i in range(740, 750):
-        #     for j in range(220, 260):
-        #         data[i, j] =0
-        #
-        # image.show()
-
-
-
-
-
-
-    # if isCollid(data):
-    #     hit("up")
+       
